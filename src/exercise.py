e77926fbc034dd8afaf38e360b251aa7e5bfe264
alau740 <77434904+alau740@users.noreply.github.com>
2021-11-01 22:31:58 +0000

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    num = int(input("enter number"))
+    if num < 1:
+        num2 = num * -1
+        print(num2)
+    else:
+        print(num)
 
 if __name__ == '__main__':
     main()
